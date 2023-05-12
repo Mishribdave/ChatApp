eea0b2bc1435f4d34d225be5eb05e342b9467085
Mishribdave <121012141+Mishribdave@users.noreply.github.com>
2023-05-12 12:27:58 +0530

    updated

--- chatapp/lib/responsive/responsive.dart
+++ chatapp/lib/responsive/responsive.dart
@@ -1,19 +1,23 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/rendering.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-// class ResponsiveLayout extends StatelessWidget {
-//   final Widget mobileScreenLayout;
-//   final Widget webScreenLayout;
-//   const ResponsiveLayout({Key? key, required this.mobileScreenLayout, required this.webScreenLayout}):super(key: key)
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileScreenLayout;
+  final Widget webScreenLayout;
+  const ResponsiveLayout(
+      {Key? key,
+      required this.mobileScreenLayout,
+      required this.webScreenLayout})
+      : super(key: key);
 
-//   // ignore: empty_constructor_bodies
-//   @override
-//   Widget build(BuildContext context) {
-//     return LayoutBuilder(builder: (context, constraints) {
-//       if(constraints.maxWidth>900){
-//         return webScreenLayout;
-//       }
-//       return mobileScreenLayout;
-//     });
-//   }
-// }
+  // ignore: empty_constructor_bodies
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > 900) {
+        return webScreenLayout;
+      }
+      return mobileScreenLayout;
+    });
+  }
+}
--- chatapp/lib/screens/mobile_screen.dart
+++ chatapp/lib/screens/mobile_screen.dart
@@ -1,1 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class mobileScreenLayout extends StatelessWidget {
+  const mobileScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color(0xff7C7B9B),
+            title: Text(
+              "ChitChat",
+              style: TextStyle(
+                color: Color(0xffFCAAAB),
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            centerTitle: false,
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.search,
+                    color: Color(0xffFCAAAB),
+                    size: 30,
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.more_vert,
+                    color: Color(0xffFCAAAB),
+                    size: 30,
+                  )),
+            ],
+            // bottom: TabBar(tabs: ),
+          ),
+        ));
+  }
+}
